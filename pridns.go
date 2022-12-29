7e3212df0de7f585de1aed00f7d213d6bad26e27
chendaiwang <chendaiwang@iboxpay.com>
2022-12-29 16:52:18 +0800

    feat: 完成配置解析

--- pridns.go
+++ pridns.go
@@ -3,14 +3,29 @@ package pridns
 import (
 	"context"
 	"github.com/coredns/coredns/plugin"
+	clog "github.com/coredns/coredns/plugin/pkg/log"
 	"github.com/coredns/coredns/request"
 	"github.com/miekg/dns"
 )
 
+var log = clog.NewWithPlugin("pridns")
+
+const (
+	storeTypeMySQL = "mysql" // 表示使用 mysql 作为存储介质
+	storeTypeEtcd  = "etcd"  // 表示使用 etcd 作为存储介质
+	storeTypeRedis = "redis" // 表示使用 redis 作为存储介质
+)
+
+type MySQLConfig struct {
+	// DataSourceName 为MySQL数据库连接地址
+	dataSourceName string
+}
+
 // Config 表示插件配置
 type Config struct {
-	// DataSourceName 为MySQL数据库连接地址
-	DataSourceName string
+	adminPassword string
+	storeType     string
+	mySQL         MySQLConfig
 }
 
 type PriDns struct {
@@ -34,10 +49,10 @@ func (d PriDns) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) 
 	m.SetReply(r)
 	m.Authoritative = true
 	m.Answer = answers
-
 	if err := w.WriteMsg(m); err != nil {
 		return dns.RcodeServerFailure, err
 	}
+
 	return dns.RcodeSuccess, nil
 }
 
--- setup.go
+++ setup.go
@@ -36,23 +36,73 @@ func parse(c *caddy.Controller) (*Config, error) {
 		}
 		i++
 
-		_ = c.RemainingArgs()
+		// 目前不需要指令
+		args := c.RemainingArgs()
+		if len(args) != 0 {
+			return nil, c.Errf("Wrong argument count or unexpected line ending after '%s'", args[0])
+		}
 
-		for c.NextBlock() {
-			switch c.Val() {
-			case "dataSourceName":
-				dataSourceNameArgs := c.RemainingArgs()
-				if len(dataSourceNameArgs) != 1 {
-					return nil, c.Errf("dataSourceName 配置错误")
+		// 进入到配置块中（由于 caddyfile.Dispenser 不支持嵌套块，所以这里不能使用 NextBlock()）
+		if c.Next() && c.Val() == "{" {
+			for c.Next() && c.Val() != "}" {
+				switch c.Val() {
+				case "adminPassword":
+					adminPasswordArgs := c.RemainingArgs()
+					if len(adminPasswordArgs) != 1 {
+						return nil, c.Err("'adminPassword' 配置错误，它有且仅有一个参数")
+					}
+					config.adminPassword = adminPasswordArgs[0]
+				case "mysql":
+					if config.storeType != "" {
+						return nil, c.Err("配置重复定义: mysql")
+					}
+					config.storeType = storeTypeMySQL
+
+					for c.NextBlock() {
+						switch c.Val() {
+						case "dataSourceName":
+							dataSourceNameArgs := c.RemainingArgs()
+							if len(dataSourceNameArgs) != 1 {
+								return nil, c.Errf("dataSourceName 配置错误")
+							}
+							config.mySQL.dataSourceName = dataSourceNameArgs[0]
+						default:
+							return nil, c.Errf("不支持的配置: %s", c.Val())
+						}
+					}
+				case "etcd":
+					if config.storeType != "" {
+						return nil, c.Err("配置重复定义: etcd")
+					}
+					config.storeType = storeTypeEtcd
+
+					for c.NextBlock() {
+						switch c.Val() {
+						default:
+							return nil, c.Errf("不支持的配置: %s", c.Val())
+						}
+					}
+				case "redis":
+					if config.storeType != "" {
+						return nil, c.Err("配置重复定义: redis")
+					}
+					config.storeType = storeTypeRedis
+
+					for c.NextBlock() {
+						switch c.Val() {
+						default:
+							return nil, c.Errf("不支持的配置: %s", c.Val())
+						}
+					}
+				default:
+					return nil, c.Errf("不支持的配置: %s", c.Val())
 				}
-				config.DataSourceName = dataSourceNameArgs[0]
-			default:
 			}
 		}
 	}
 
-	if config.DataSourceName == "" {
-		return nil, c.Errf("必要不能配置为空")
+	if config.storeType == "" {
+		return nil, c.Errf("必须至少使用其中一种存储")
 	}
 
 	return config, nil
