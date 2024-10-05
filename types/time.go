08c610260be26bf83c556ed825acfeea2249a18f
Laeni <m@laeni.cn>
2024-10-06 01:55:43 +0800

    fix: 修复 grm 框架无法识别自定义时间类型

--- types/time.go
+++ types/time.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"database/sql/driver"
 	"fmt"
 	"time"
 )
@@ -12,4 +13,19 @@ func (t *LocalTime) MarshalJSON() ([]byte, error) {
 	format := time.Time(*t).Format("2006-01-02 15:04:05")
 	// 由于序列化之后为字符串，所以必须加引号
 	return []byte(fmt.Sprintf("\"%s\"", format)), nil
+}
+
+// 实现 Scanner 接口
+func (t *LocalTime) Scan(src interface{}) error {
+	t2, ok := src.(time.Time)
+	if !ok {
+		return fmt.Errorf("cannot convert %v to LocalTime", src)
+	}
+	*t = LocalTime(t2)
+	return nil
+}
+
+// 实现 Valuer 接口
+func (t LocalTime) Value() (driver.Value, error) {
+	return time.Time(t), nil
 }
